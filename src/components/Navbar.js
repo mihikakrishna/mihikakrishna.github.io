d184ee056743acda48eca4a605aff79c2bd65eb8
mihikakrishna <krishna.mihika@gmail.com>
2023-10-07 14:46:05 -0700

    Configure React app for deployment to GitHub Pages

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import React  from 'react';
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <CustomLink to="/">Home</CustomLink>
+      <CustomLink to="/about">About</CustomLink>
+      <CustomLink to="/skills">Skills</CustomLink>
+      <CustomLink to="/education">Education</CustomLink>
+      <CustomLink to="/experience">Experience</CustomLink>
+      <CustomLink to="/projects">Projects</CustomLink>
+    </nav>
+  )
+}
+
+function CustomLink({ to, children }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  let pageClass = to.replace('/', ''); // Remove leading slash to get page name
+
+  if (pageClass === '') {
+    pageClass = 'home';
+  }
+
+  return (
+    <div className={`nav-link ${isActive ? 'active' : ''} ${pageClass}`}>
+      <Link to={to}>
+        {children}
+      </Link>
+    </div>
+  );
+}
+
+
+
---
+++ src/pages/Education.js
@@ -0,0 +1,4 @@
+import React  from 'react';
+export default function Skills() {
+  return <h1>Education</h1>
+}
