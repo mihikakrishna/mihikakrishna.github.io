34cd1c8bde3ecae207236f273f74402263cb42c3
mihikakrishna <krishna.mihika@gmail.com>
2023-10-13 16:48:24 -0700

    Modify about-me styling and content

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,10 +7,16 @@ export default function About() {
       <div className="glassmorphism-effect">
         <div className="content-box">
           <h4>👋 Hello World, I'm Mihika!</h4>
-          <p>I'm kinda obsessed with coding. Not in the "I dream in binary" way, but more like "I love making cool stuff happen on a screen." It's where I get to flex my problem-solving muscles and let my creativity run wild.</p>
+          <p style={{ letterSpacing: '1px'}}>I'm obsessed with coding. Not in a "I dream in binary" way, but more like "I love making cool stuff happen on a screen." It's where I get to flex my problem-solving muscles and let my creativity run wild.</p>
           <p>A few things about me:</p>
-          <ul style={{ textAlign: 'left' }}>
-              <li>🚀 Coding: Started because I wanted to make video games in highschool. Now, I can't get enough of turning ideas into reality.</li>
+          <ul style={{ textAlign: 'left', color: '#d4c7ec', listStyleType: 'none'}}>
+          <li>
+          🚀 Coding:
+              Started because I wanted to make video games in highschool. <br></br>
+            <div style={{marginLeft: '2em'}}>
+              Now, I can't get enough of turning ideas into reality!
+            </div>
+          </li>
               <li>🎸 Guitar: I love jamming out to my favorite songs. It helps me relax when I'm stressed.</li>
               <li>♟️ Chess: It's the perfect break from screens. Plus, I'm trying to get to 1000 elo.</li>
               <li>✍️ Drawing: It’s where I zone out and let my hands do the thinking.</li>
