8a50a59da1a92be29ba6485feec369c7264c97fb
mihikakrishna <krishna.mihika@gmail.com>
2023-10-13 22:55:37 -0700

    Add project carousel

--- src/components/FadingImage.js
+++ src/components/FadingImage.js
@@ -1,33 +1,38 @@
 import React, { useEffect, useRef } from 'react';
 
-function FadingImage({ src, alt }) {
-    const imageRef = useRef(null);
+function FadingImage({ src, alt, width, height }) {
+  const imageRef = useRef(null);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if (!imageRef.current) return;
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!imageRef.current) return;
 
-            const rect = imageRef.current.getBoundingClientRect();
-            const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+      const rect = imageRef.current.getBoundingClientRect();
+      const windowHeight = window.innerHeight || document.documentElement.clientHeight;
 
-            // Check if the image is within the viewport
-            if (rect.top <= windowHeight && rect.bottom >= 0) {
-                imageRef.current.style.opacity = 1;
-            } else {
-                imageRef.current.style.opacity = 0;
-            }
-        };
+      // Check if the image is within the viewport
+      if (rect.top <= windowHeight && rect.bottom >= 0) {
+        imageRef.current.style.opacity = 1;
+      } else {
+        imageRef.current.style.opacity = 0;
+      }
+    };
 
-        window.addEventListener('scroll', handleScroll);
-        handleScroll();  // Initial check
+    window.addEventListener('scroll', handleScroll);
+    handleScroll(); // Initial check
 
-        // Cleanup on component unmount
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+    // Cleanup on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
-    return <img ref={imageRef} className="fade-in-out" src={src} alt={alt} />;
+  const imageStyle = {
+    width: width || 'auto', // Set the width (default to 'auto' if not provided)
+    height: height || 'auto', // Set the height (default to 'auto' if not provided)
+  };
+
+  return <img ref={imageRef} className="fade-in-out" src={src} alt={alt} style={imageStyle} />;
 }
 
 export default FadingImage;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,4 +1,40 @@
-import React  from 'react';
-export default function Skills() {
-  return <h1>Projects</h1>
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import carousel styles
+
+import Btree from '../assets/images/btree.png';
+import GpaAalysis from '../assets/images/davis-gpa-analysis.png';
+import HealthTracker from '../assets/images/health-tracker-project.png';
+import SlidingPuzzle from '../assets/images/sliding-puzzle-project.png';
+import HandGestureRecognizer from '../assets/images/hand-gesture-project.png';
+
+export default function Projects() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <Carousel>
+        <div>
+          <img src={HealthTracker} alt="Health Tracker Project" width="400px" height="400px" />
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+          <img src={HandGestureRecognizer} alt="Hand Gesture Recognizer Project" width="400px" height="400px" />
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+            <img src={Btree} alt="Btree Project" width="400px" height="400px"/>
+            <p className="legend">Legend 1</p>
+        </div>
+        <div>
+          <img src={GpaAalysis} alt="Davis GPA Analysis Project" width="400px" height="400px" />
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+          <img src={SlidingPuzzle} alt="Sliding Puzzle Project" width="400px" height="400px" />
+          <p className="legend">Legend 1</p>
+        </div>
+        {/* Add more carousel items as needed */}
+      </Carousel>
+    </div>
+  );
 }
