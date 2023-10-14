dcf910ba9dcfbbb75d7720139c6cc254f00c97f6
mihikakrishna <krishna.mihika@gmail.com>
2023-10-13 17:00:23 -0700

    Add about-me typing effect

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 export default function About() {
+  const typewriterRef = useRef(null);
+
+  useEffect(() => {
+    const element = typewriterRef.current;
+    const fullText = element.textContent;
+    element.textContent = '';
+
+    let i = 0;
+    const typingInterval = setInterval(() => {
+      if (i < fullText.length) {
+        element.textContent += fullText[i];
+        i++;
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, 100); 
+
+    return () => clearInterval(typingInterval);
+  }, []);
+
   return (
     <div>
       <h1>About</h1>
       <div className="glassmorphism-effect">
         <div className="content-box">
-          <h4>👋 Hello World, I'm Mihika!</h4>
+          <h4 ref={typewriterRef} className="typewriter" style={{ color: '#d4c7ec', fontSize: '200%'}}>Hello World, I'm Mihika !</h4>
           <p style={{ letterSpacing: '1px'}}>I'm obsessed with coding. Not in a "I dream in binary" way, but more like "I love making cool stuff happen on a screen." It's where I get to flex my problem-solving muscles and let my creativity run wild.</p>
           <p>A few things about me:</p>
           <ul style={{ textAlign: 'left', color: '#d4c7ec', listStyleType: 'none'}}>
           <li>
           🚀 Coding:
               Started because I wanted to make video games in highschool. <br></br>
-            <div style={{marginLeft: '2em'}}>
+            <div style={{marginLeft: '1.5em'}}>
               Now, I can't get enough of turning ideas into reality!
             </div>
           </li>
