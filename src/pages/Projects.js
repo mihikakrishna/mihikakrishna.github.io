d9056aa0bd39ac22472d64c7f8fde7796f1327a1
Mihika Krishna <krishna.mihika@gmail.com>
2024-02-17 11:16:08 -0800

    Add project titles in project page. Remove background colors

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,18 +4,17 @@ import HealthTracker from '../assets/images/health-tracker-project.png';
 import SlidingPuzzle from '../assets/images/sliding-puzzle-project.png';
 import HandGestureRecognizer from '../assets/images/hand-gesture-project.png';
 
-const ProjectSection = ({ image, altText, href, description, backgroundColor, textColor }) => (
-    <div className="section" style={{ backgroundColor: backgroundColor }}>
-      <a href={href} target="_blank" rel="noopener noreferrer">
-        <img src={image} alt={altText} />
-        <div className="description" style={{ color: textColor }}>
+const ProjectSection = ({ image, altText, href, description, backgroundColor, textColor, title }) => (
+    <div className="section" >
+      <div style={{ fontSize: '2em', color: '#FFFFFF'}}>{title}</div>
+      <a href={href} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'inherit' }}>
+        <img src={image} alt={altText} style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', marginTop: 'auto', marginBottom: 'auto', maxHeight: '100%', maxWidth: '100%' }} />
+        <div className="description" style={{ color: textColor, textAlign: 'center' }}>
           <p>{description}</p>
         </div>
       </a>
     </div>
   );
-  
-  
 
 export default function Projects() {
   useEffect(() => {
@@ -43,6 +42,7 @@ export default function Projects() {
           description="A web Application designed to track your fitness journey."
           backgroundColor="#57e2e5"
           textColor="#FFFFFF"
+          title="Health Tracker"
         />
         <ProjectSection 
           image={HandGestureRecognizer} 
@@ -51,6 +51,7 @@ export default function Projects() {
           description="A web application that accurately recognizes and categorizes various hand gestures in real-time."
           backgroundColor="#e08dac"
           textColor="#FFFFFF"
+          title="Hand Gesture Recognizer"
         />
         <ProjectSection 
           image={SlidingPuzzle} 
@@ -59,6 +60,7 @@ export default function Projects() {
           description="A program that solves any manually shuffled sliding puzzle configuration using DFS, BFS, and A*, and compares stats."
           backgroundColor="#45cb85"
           textColor="#FFFFFF"
+          title="Sliding Puzzle Solver"
         />
         <ProjectSection 
           image={GpaAalysis} 
@@ -67,6 +69,7 @@ export default function Projects() {
           description="A Python script for scraping and extracting UC Davis GPA data."
           backgroundColor="#f0abff"
           textColor="#FFFFFF"
+          title="UC Davis GPA Analysis Tool"
         />
       </div>
     </div>
