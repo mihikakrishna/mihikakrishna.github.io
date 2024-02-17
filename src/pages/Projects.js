fc658de12914e3dfc1a7aa28c4d73d531257421b
Mihika Krishna <krishna.mihika@gmail.com>
2024-02-17 09:48:44 -0800

    Update projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,23 +1,21 @@
 import React, { useEffect } from "react";
-import Btree from '../assets/images/btree.png';
 import GpaAalysis from '../assets/images/davis-gpa-analysis.png';
 import HealthTracker from '../assets/images/health-tracker-project.png';
 import SlidingPuzzle from '../assets/images/sliding-puzzle-project.png';
 import HandGestureRecognizer from '../assets/images/hand-gesture-project.png';
 
-const ProjectSection = ({ image, altText, href, description, backgroundColor }) => (
-  <div className="section" style={{ backgroundColor: backgroundColor }}>
-    <div className="section-content">
-      <img src={image} alt={altText} />
+const ProjectSection = ({ image, altText, href, description, backgroundColor, textColor }) => (
+    <div className="section" style={{ backgroundColor: backgroundColor }}>
+      <a href={href} target="_blank" rel="noopener noreferrer">
+        <img src={image} alt={altText} />
+        <div className="description" style={{ color: textColor }}>
+          <p>{description}</p>
+        </div>
+      </a>
     </div>
-    <p>{description}</p>
-    <a href={href} target="_blank" rel="noopener noreferrer" className="source-link">
-      <p className="legend">Source Code</p>
-    </a>
-  </div>
-);
-
-
+  );
+  
+  
 
 export default function Projects() {
   useEffect(() => {
@@ -42,32 +40,33 @@ export default function Projects() {
           image={HealthTracker} 
           altText="Health Tracker Project" 
           href="https://github.com/mihikakrishna/HealthTracker" 
-          description="A comprehensive health tracker for individuals."
+          description="A web Application designed to track your fitness journey."
           backgroundColor="#57e2e5"
+          textColor="#FFFFFF"
         />
         <ProjectSection 
           image={HandGestureRecognizer} 
           altText="Hand Gesture Recognizer Project" 
           href="https://github.com/mihikakrishna/Hand-Gesture-Recognizer" 
+          description="A web application that accurately recognizes and categorizes various hand gestures in real-time."
           backgroundColor="#e08dac"
-        />
-        <ProjectSection 
-          image={Btree} 
-          altText="Btree Project" 
-          href="https://github.com/mihikakrishna/Btree"
-          backgroundColor="#6a7fdb"
-        />
-        <ProjectSection 
-          image={GpaAalysis} 
-          altText="Davis GPA Analysis Project" 
-          href="https://github.com/mihikakrishna/UC-Davis-GPA-Analysis-Tool" 
-          backgroundColor="#45cb85"
+          textColor="#FFFFFF"
         />
         <ProjectSection 
           image={SlidingPuzzle} 
           altText="Sliding Puzzle Project" 
-          href="https://github.com/mihikakrishna/sliding-puzzle" 
-          backgroundColor="#153131"
+          href="https://github.com/mihikakrishna/sliding-puzzle"
+          description="A program that solves any manually shuffled sliding puzzle configuration using DFS, BFS, and A*, and compares stats."
+          backgroundColor="#45cb85"
+          textColor="#FFFFFF"
+        />
+        <ProjectSection 
+          image={GpaAalysis} 
+          altText="Davis GPA Analysis Project" 
+          href="https://github.com/mihikakrishna/UC-Davis-GPA-Analysis-Tool"
+          description="A Python script for scraping and extracting UC Davis GPA data."
+          backgroundColor="#f0abff"
+          textColor="#FFFFFF"
         />
       </div>
     </div>
